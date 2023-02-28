ee1137ddb164dfbb282ac10e347d6edca6f43c67
Chenyang Zhong <zhongcy95@gmail.com>
2023-02-28 09:04:23 -0500

    Revert "input: touchscreen: fts_spi: trigger fod hbm"
    This reverts commit 87ec1f7170e38067aeb4e6e534b67eb608b18729.

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -55,7 +55,7 @@
 #include <linux/notifier.h>
 #include <linux/backlight.h>
 #include <drm/mi_disp_notifier.h>
-#include <drm/dsi_display_fod.h>
+
 #include <linux/fb.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
@@ -164,7 +164,6 @@ void release_all_touches(struct fts_ts_info *info)
 	input_sync(info->input_dev);
 	input_report_key(info->input_dev, BTN_INFO, 0);
 	input_sync(info->input_dev);
-	dsi_display_primary_request_fod_hbm(0);
 	info->touch_id = 0;
 	info->touch_skip = 0;
 	info->fod_id = 0;
@@ -3552,15 +3551,11 @@ static void fts_enter_pointer_event_handler(struct fts_ts_info *info,
 				__set_bit(touchId, &info->fod_id);
 				input_report_abs(info->input_dev, ABS_MT_WIDTH_MINOR, info->fod_overlap);
 				input_report_key(info->input_dev, BTN_INFO, 1);
-				input_sync(info->input_dev);
-				dsi_display_primary_request_fod_hbm(1);
 				logError(1,	"%s  %s :  FOD Press :%d, fod_id:%08x\n", tag, __func__, touchId, info->fod_id);
 			}
 		} else if (__test_and_clear_bit(touchId, &info->fod_id)) {
 			input_report_abs(info->input_dev, ABS_MT_WIDTH_MINOR, 0);
 			input_report_key(info->input_dev, BTN_INFO, 0);
-			input_sync(info->input_dev);
-			dsi_display_primary_request_fod_hbm(0);
 			info->fod_x = 0;
 			info->fod_y = 0;
 			info->fod_coordinate_update = false;
@@ -3662,8 +3657,6 @@ static void fts_leave_pointer_event_handler(struct fts_ts_info *info,
 	if (__test_and_clear_bit(touchId, &info->fod_id)) {
 		input_report_abs(info->input_dev, ABS_MT_WIDTH_MINOR, 0);
 		input_report_key(info->input_dev, BTN_INFO, 0);
-		input_sync(info->input_dev);
-		dsi_display_primary_request_fod_hbm(0);
 		info->fod_coordinate_update = false;
 		info->fod_x = 0;
 		info->fod_y = 0;
@@ -3679,8 +3672,6 @@ static void fts_leave_pointer_event_handler(struct fts_ts_info *info,
 		info->fod_y = 0;
 		sysfs_notify(&info->fts_touch_dev->kobj, NULL, dev_attr_fod_state.attr.name);
 		input_report_key(info->input_dev, BTN_INFO, 0);
-		input_sync(info->input_dev);
-		dsi_display_primary_request_fod_hbm(0);
 
 #ifdef FTS_XIAOMI_TOUCHFEATURE
 		wake_up(&info->wait_queue);
@@ -4064,7 +4055,6 @@ static void fts_gesture_event_handler(struct fts_ts_info *info,
 					sysfs_notify(&info->fts_touch_dev->kobj, NULL, dev_attr_fod_state.attr.name);
 					input_report_key(info->input_dev, BTN_INFO, 1);
 					input_sync(info->input_dev);
-					dsi_display_primary_request_fod_hbm(1);
 					if (info->fod_id) {
 						fod_id = ffs(info->fod_id) - 1;
 						if (info->fod_id & ~(1 << fod_id))
